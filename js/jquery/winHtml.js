9b9fc8735a4a54d96520b88e162dec720fd01a29
Laid <weinet.laid@gmail.com>
2019-10-07 15:54:39 +0800

    国庆 最新

--- js/jquery/winHtml.js
+++ js/jquery/winHtml.js
@@ -5,18 +5,18 @@ var html='<div class="pop-content hide" data-cashap-id="pop-content">' +
     '                <img src="../style/img/ic_close.png" alt="">' +
     '            </div>' +
     '            <div class="content">' +
-    '                <div class="pop-title green-text">' +
+    '                <div class="pop-title green-text" text-switch="text-switch">' +
     '                    <span>风格切换</span>' +
     '                </div>' +
     '                <div class="pop-active" data-btnAll="data-btnAll">' +
-    '                    <div class="pop-btn green-btn " data-btn="data-btn"></div>' +
-    '                    <div class="pop-btn blue-btn" data-btn="data-btn"></div>' +
-    '                    <div class="pop-btn orange-btn" data-btn="data-btn"></div>' +
+    '                    <div class="pop-btn green-btn " ></div>' +
+    '                    <div class="pop-btn blue-btn" ></div>' +
+    '                    <div class="pop-btn orange-btn" ></div>' +
     '                </div>' +
-    '                <div class="pop-img">' +
-    '                    <div class="green-switch" data-tab-switch="data-tab-switch"></div>' +
-    '                    <div class="blue-switch" data-tab-switch="data-tab-switch"></div>' +
-    '                    <div class="orange-switch" data-tab-switch="data-tab-switch"></div>' +
+    '                <div class="pop-img"  theme-switch="theme-switch">' +
+    '                    <div class="green-switch" ></div>' +
+    '                    <div class="blue-switch" ></div>' +
+    '                    <div class="orange-switch"></div>' +
     '                </div>' +
     '                <div class="pop-event">' +
     '                    <div class="btn-submit green-btn" data-save="data-save" data-submit="data-submit" >存储</div>' +
